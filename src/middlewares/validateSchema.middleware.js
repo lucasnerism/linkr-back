8750ae26e84b647f8e4ce9ebdb879c3faa7b55d9
Lucas Neris <lucasnerism@gmail.com>
2023-05-31 04:10:00 -0300

    find users using search and by id

--- src/middlewares/validateSchema.middleware.js
+++ src/middlewares/validateSchema.middleware.js
@@ -1,11 +1,13 @@
+import sanitizeObjects from "../helpers/sanitizeObjects.helper.js";
+
 export function validateSchema(schema) {
     return (req, res, next) => {
-        const validation = schema.validate(req.body, { abortEarly: false });
+        const validation = schema.validate(sanitizeObjects(req.body), { abortEarly: false });
 
         if (validation.error) {
             const errors = validation.error.details.map(detail => detail.message);
             return res.status(422).send(errors);
         }
         next();
-    }
+    };
 }
