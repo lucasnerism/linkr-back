3cb96d2652d830e5c1621cb174d67e6b6b74c903
Lucas Neris <lucasnerism@gmail.com>
2023-06-03 03:34:51 -0300

    added token validation to all /posts routes

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -6,10 +6,10 @@ import { validateToken } from "../middlewares/validateToken.middleware.js";
 
 const postsRouter = Router();
 
-postsRouter.post('/posts', validateSchema(postSchema), postsController.createPost);
-postsRouter.get('/posts', postsController.getPosts);
-postsRouter.get('/hashtag', postsController.getTrendingHashtags);
-postsRouter.get('/hashtag/:hashtag', postsController.getPostsByHashtag);
+postsRouter.post('/posts', validateSchema(postSchema), validateToken, postsController.createPost);
+postsRouter.get('/posts', validateToken, postsController.getPosts);
+postsRouter.get('/hashtag', validateToken, postsController.getTrendingHashtags);
+postsRouter.get('/hashtag/:hashtag', validateToken, postsController.getPostsByHashtag);
 postsRouter.post('/post/:id/like', validateToken, postsController.likePost);
 postsRouter.post('/post/:id/dislike', validateToken, postsController.dislikePost);
 
