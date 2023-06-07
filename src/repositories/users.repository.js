ed4e5c9cef6d9dbe639f310d74d7bb1305bd3a5f
Lucas Neris <lucasnerism@gmail.com>
2023-06-07 00:41:52 -0300

    fix to queries not working with empty posts

--- src/repositories/users.repository.js
+++ src/repositories/users.repository.js
@@ -3,39 +3,47 @@ import { db } from "../database/connect.js";
 const getUserById = (id, userId) => {
   return db.query(`
   SELECT
-    u.id,u.name,u.profile_picture AS "image", EXISTS(SELECT * FROM follows WHERE follower_id = $2 and following_id = u.id) AS following,
-    JSON_AGG(
-      JSON_BUILD_OBJECT(
-        'id',p.id,
-        'userId',p.user_id,
-        'link',p.link,
-        'comment',p.comment,
-        'title',p.title,
-        'description',p.description,
-        'image',p.image,
-        'hashtags',(COALESCE(h.hashtag,'[]')),
-        'likes',l.*        
-      )
-    ) AS posts
+    u.id,u.name,u.profile_picture AS "image", EXISTS(SELECT * FROM follows WHERE follower_id = $2 and following_id = $1) AS following,
+    COALESCE(p.posts,'[]') AS posts
   FROM
-    users u
-  INNER JOIN
-    posts p ON p.user_id = u.id
+    users u  
   CROSS JOIN LATERAL(
-    SELECT JSON_AGG(h.name) AS hashtag
-    FROM hashtags h
-    WHERE post_id = p.id
-  ) h
-  CROSS JOIN LATERAL(
-    SELECT COALESCE(COUNT(l.id),0) AS "total", COALESCE(JSON_AGG(JSON_BUILD_OBJECT('id',u.id,'name',u.name)),'[]') AS "users"
-    FROM likes l
-    JOIN users u ON l.user_id = u.id
-    WHERE post_id=p.id    
-  ) l
+    SELECT
+      JSONB_AGG(
+        JSONB_BUILD_OBJECT(
+          'id',p.id,
+          'userId',p.user_id,
+          'link',p.link,
+          'comment',p.comment,
+          'title',p.title,
+          'description',p.description,
+          'image',p.image,
+          'hashtags',(COALESCE(h.hashtag,'[]')),
+          'likes',l.*        
+        )
+      ORDER BY
+        p.id DESC
+      ) AS posts
+    FROM
+      posts p
+    CROSS JOIN LATERAL(
+      SELECT JSONB_AGG(h.name) AS hashtag
+      FROM hashtags h
+      WHERE post_id = p.id
+    ) h
+    CROSS JOIN LATERAL(
+      SELECT COALESCE(COUNT(l.id),0) AS "total", COALESCE(JSONB_AGG(JSONB_BUILD_OBJECT('id',u.id,'name',u.name)),'[]') AS "users"
+      FROM likes l
+      JOIN users u ON l.user_id = u.id
+      WHERE post_id=p.id    
+    ) l
+    WHERE
+      p.user_id=$1    
+  ) p
   WHERE
-    u.id=$1
+    u.id=$1  
   GROUP BY
-    u.id,u.name,u.profile_picture
+    u.id,u.name,u.profile_picture,p.posts
   ;`, [id, userId]);
 };
 
