4f21dc8fc2099c7092acf829badc336214b4998d
ysnsales <ysnsaless@gmail.com>
2023-06-09 16:32:33 -0300

    feat: commments

--- src/repositories/comments.repository.js
+++ src/repositories/comments.repository.js
@@ -11,11 +11,11 @@ const createComment = (comment, user_id, post_id) => {
 
 const getPostComments = (post_id) => {
     return db.query(`
-    SELECT * FROM
-     comments
-    WHERE 
-     post_id = $1
-    ;`, [post_id] )
+    SELECT comments.*, users.name, users.profile_picture
+    FROM comments
+    INNER JOIN users ON comments.user_id = users.id
+    WHERE comments.post_id = $1;
+  `, [post_id]);
 };
 
 export default {
